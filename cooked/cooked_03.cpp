f21472ef8ec96ab2971d263a024b26e099c771a8
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2025-01-06 18:22:11 +0300

    Update cooked_03.cpp

--- cooked/cooked_03.cpp
+++ cooked/cooked_03.cpp
@@ -4,14 +4,14 @@
 
 namespace Nec
 {
-	constexpr size_t operator "" _KB(unsigned long long size)
+	constexpr std::size_t operator "" _KB(unsigned long long size)
 	{
-		return static_cast<size_t>(size * 1024);
+		return static_cast<std::size_t>(size * 1024);
 	}
 
-	constexpr size_t operator "" _MB(unsigned long long size)
+	constexpr std::size_t operator "" _MB(unsigned long long size)
 	{
-		return static_cast<size_t>(size * 1024 * 1024);
+		return static_cast<std::size_t>(size * 1024 * 1024);
 	}
 }
 
@@ -19,7 +19,8 @@ namespace Nec
 int main()
 {
 	using namespace Nec;
-	auto size{ 4_KB };         // size_t size = 4096; 
+
+	auto size{ 4_KB };         // std::size_t size = 4096; 
 
 	using byte = unsigned char;
 	auto buf1 = std::array<byte, 16_KB>{};
